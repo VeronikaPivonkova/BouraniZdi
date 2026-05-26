9e18eab96068336b07349aaf37610ba8b814051e
pleslea20v-ops <pleslea20v@gyri.cz>
2026-05-26 13:08:55 +0200

    Přidejte soubory projektu.

---
+++ BouraniZdi/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace BouraniZdi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbPlatno = new System.Windows.Forms.PictureBox();
+            this.tmrVykreslení = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPlatno
+            // 
+            this.pbPlatno.BackColor = System.Drawing.Color.White;
+            this.pbPlatno.Location = new System.Drawing.Point(3, 12);
+            this.pbPlatno.Name = "pbPlatno";
+            this.pbPlatno.Size = new System.Drawing.Size(771, 445);
+            this.pbPlatno.TabIndex = 0;
+            this.pbPlatno.TabStop = false;
+            this.pbPlatno.Click += new System.EventHandler(this.pbPlatno_Click);
+            // 
+            // tmrVykreslení
+            // 
+            this.tmrVykreslení.Tick += new System.EventHandler(this.tmrVykreslení_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbPlatno);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hra Bourání zdi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPlatno;
+        private System.Windows.Forms.Timer tmrVykreslení;
+    }
+}
+
---
+++ BouraniZdi/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouraniZdi
+{
+    public partial class Form1 : Form
+    {
+        // objekt grafiky pro kreslení
+
+        Graphics mobjGrafika;
+
+        //hodnoty kuličky
+        int mintSouradniceX, mintSouradniceY;
+        int mintPosunX, mintPosunY;
+        const int cnVelikost = 20;
+
+
+        //------------------------------------------------
+        // konstruktor
+        //------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //------------------------------------------------
+        // nahrání folmuláře
+        //------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // připojení grafiky na picturebox
+            mobjGrafika = pbPlatno.CreateGraphics();
+
+            // nastavit kuličku
+            mintSouradniceX = mintSouradniceY = 100;
+            mintPosunX = mintPosunY = 3;
+
+            // nastartovat timer
+            tmrVykreslení.Interval = 20;
+            tmrVykreslení.Start();
+        }
+
+
+
+        
+
+
+        private void pbPlatno_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        //------------------------------------------------
+        // vykreslování hry
+        //------------------------------------------------
+
+        private void tmrVykreslení_Tick(object sender, EventArgs e)
+        {
+            //smazání kuličky
+            mobjGrafika.FillEllipse(Brushes.White, mintSouradniceX, mintSouradniceY, cnVelikost, cnVelikost);
+
+            //posun kuličky
+            mintSouradniceX = mintSouradniceX + mintPosunX;
+            mintSouradniceY = mintSouradniceY + mintPosunY;
+
+            // odraz v Y
+            if ((mintSouradniceY + cnVelikost > pbPlatno.Height) || (mintSouradniceY < 0))
+                mintPosunY = mintPosunY * (-1);
+
+            // odraz v X
+            if ((mintSouradniceX + cnVelikost > pbPlatno.Width) || (mintSouradniceX < 0))
+                mintPosunX = mintPosunX * (-1);
+
+            // nakreslení kuličky
+            mobjGrafika.FillEllipse(Brushes.Blue, mintSouradniceX, mintSouradniceY, cnVelikost, cnVelikost);
+        }
+    }
+}
